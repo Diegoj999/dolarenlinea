0c9d24d60ee4cac5b0fcb528047eb4e3be985144
Diego <diegoj9999@gmail.com>
2023-04-26 17:12:18 -0300

    Probando

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Suspense } from "react";
-import { Loading } from "./components/Loading";
+import { Loading } from "../src/components/Loading";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Suspense fallback={<Loading />}>
